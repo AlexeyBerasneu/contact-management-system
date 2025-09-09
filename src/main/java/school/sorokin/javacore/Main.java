44cab2d325b5992dd32003e77c85033c0aa0a87f
Alexey Berasneu <alexeyberasneu@gmail.com>
2025-09-09 18:31:57 -0400

    fix : create constant move code from case to private static method refact: method validName

--- src/main/java/school/sorokin/javacore/Main.java
+++ src/main/java/school/sorokin/javacore/Main.java
@@ -3,11 +3,16 @@ package school.sorokin.javacore;
 import java.util.Scanner;
 
 public class Main {
+    private static int quantity = 1000;
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final String wasNotFound = "The contact with name %s  was not found.";
+    private static final String enterName = "Enter contact name: ";
+    private static String[] name = new String[quantity];
+    private static String[] phone = new String[quantity];
+    private static int count = 0;
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String[] name = new String[1000];
-        String[] phone = new String[1000];
-        int count = 0;
+
         while (true) {
             System.out.println();
             printMenu();
@@ -15,73 +20,19 @@ public class Main {
             switch (input.trim()) {
                 // add new contact
                 case "1" -> {
-                    if (count < 1000) {
-                        name[count] = validName().trim();
-                        //validate phone number
-                        phone[count] = validPhoneNumber().trim();
-                        System.out.println("Contact has been saved.");
-                        count++;
-                    } else {
-                        System.out.println("List of contacts is full");
-                    }
+                    addContact();
                 }
                 // show all contacts
                 case "2" -> {
-                    if (count == 0) {
-                        System.out.println("List of contacts is empty");
-                    } else {
-                        for (int i = 0; i <= count - 1; i++) {
-                            System.out.println(i + 1 + "." + name[i] + " - " + phone[i]);
-                        }
-                    }
+                    shoAllContacts();
                 }
                 //find by name
                 case "3" -> {
-                    System.out.print("Enter contact name: ");
-                    String str = scanner.nextLine();
-                    int temp = 0;
-                    for (int i = 0; i < count; i++) {
-                        //to be sure that you didn't make name mistake
-                        if (name[i].toLowerCase().equals(str.toLowerCase().trim())) {
-                            System.out.println("The phone number of " + name[i] + " is : " + phone[i]);
-                            temp++;
-                        }
-                    }
-                    if (temp == 0) {
-                        System.out.println("The contact with name " + str + " was not found.");
-                    }
+                    findByName();
                 }
                 //delete contact
                 case "4" -> {
-                    System.out.print("Enter contact name for deleting: ");
-                    String str = scanner.nextLine();
-                    int temp = 0;
-                    for (int i = 0; i < count; i++) {
-                        if (name[i].toLowerCase().equals(str.toLowerCase().trim())) {
-                            name[i] = null;
-                            phone[i] = null;
-                            temp++;
-                        }
-                    }
-                    if (temp == 0) {
-                        System.out.println("The contact with name " + str + " was not found.");
-                    } else {
-                        System.out.println("The contact with name " + str + " has been deleted.");
-                        // shift the array after removing contacts
-                        for (int i = 0; i < count - 1; i++) {
-                            for (int j = 0; j < count - 1; j++) {
-                                if (name[j] == null) {
-                                    String tempName = name[j];
-                                    String tempPhone = phone[j];
-                                    name[j] = name[j + 1];
-                                    name[j + 1] = tempName;
-                                    phone[j] = phone[j + 1];
-                                    phone[j + 1] = tempPhone;
-                                }
-                            }
-                        }
-                    }
-                    count = count - temp;
+                    deleteContact();
                 }
                 // exit from program
                 case "5" -> {
@@ -94,24 +45,85 @@ public class Main {
         }
     }
 
+    private static void addContact() {
+        if (count < quantity) {
+            name[count] = validName();
+            //validate phone number
+            phone[count] = validPhoneNumber().trim();
+            System.out.println("Contact has been saved.");
+            count++;
+        } else {
+            System.out.println("List of contacts is full");
+        }
+    }
+
+    private static void shoAllContacts() {
+        if (count == 0) {
+            System.out.println("List of contacts is empty");
+        } else {
+            for (int i = 0; i <= count - 1; i++) {
+                System.out.println(i + 1 + "." + name[i] + " - " + phone[i]);
+            }
+        }
+    }
+
+    private static void findByName() {
+        System.out.print(enterName);
+        String str = scanner.nextLine();
+        int temp = 0;
+        for (int i = 0; i < count; i++) {
+            //to be sure that you didn't make name mistake
+            if (name[i].equalsIgnoreCase(str.trim())) {
+                System.out.println("The phone number of " + name[i] + " is : " + phone[i]);
+                temp++;
+            }
+        }
+        if (temp == 0) {
+            System.out.printf(wasNotFound, str);
+        }
+    }
+
+    private static void deleteContact() {
+        System.out.print("Enter contact name for deleting: ");
+        String str = scanner.nextLine();
+        int temp = 0;
+        for (int i = 0; i < count; i++) {
+            if (name[i].equalsIgnoreCase(str.trim())) {
+                name[i] = null;
+                phone[i] = null;
+                temp++;
+            }
+        }
+        if (temp == 0) {
+            System.out.printf(wasNotFound, str);
+        } else {
+            System.out.println("The contact with name " + str + " has been deleted.");
+            // shift the array after removing contacts
+            for (int i = 0; i < count - 1; i++) {
+                for (int j = 0; j < count - 1; j++) {
+                    if (name[j] == null) {
+                        String tempName = name[j];
+                        String tempPhone = phone[j];
+                        name[j] = name[j + 1];
+                        name[j + 1] = tempName;
+                        phone[j] = phone[j + 1];
+                        phone[j + 1] = tempPhone;
+                    }
+                }
+            }
+        }
+        count = count - temp;
+    }
+
     // print menu
     private static void printMenu() {
-        System.out.print(
-                "   Menu:\n" +
-                        " 1 - Add new contact\n" +
-                        " 2 - Show all contacts\n" +
-                        " 3 - Find by name\n" +
-                        " 4 - Delete contact\n" +
-                        " 5 - Exit program\n" +
-                        "Enter number of command: "
-        );
+        System.out.print("   Menu:\n" + " 1 - Add new contact\n" + " 2 - Show all contacts\n" + " 3 - Find by name\n" + " 4 - Delete contact\n" + " 5 - Exit program\n" + "Enter number of command: ");
     }
 
     // validation phone number
     private static String validPhoneNumber() {
-        Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.print("Enter contact phone: ");
+            System.out.print(enterName);
             String number = scanner.nextLine();
             try {
                 Integer.parseInt(number);
@@ -124,10 +136,9 @@ public class Main {
 
     //validation name
     private static String validName() {
-        Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("Enter contact name: ");
-            String name = scanner.nextLine();
+            String name = scanner.nextLine().trim();
             if (!name.isEmpty()) {
                 return name;
             } else {
